a599636a8a6cf2e21f265a0b8f1e8d7888750cd1
Maksatbek Bolushov <maksat@mancho.dev>
2021-09-10 11:13:27 +0600

    before sharp added.

--- origin-response/origin-response.ts
+++ origin-response/origin-response.ts
@@ -18,5 +18,36 @@ const s3Client = new S3({
 exports.handler = async (event: CloudFrontResponseEvent) => {
   let response: CloudFrontResultResponse = event.Records[0].cf.response;
 
+  console.log(JSON.stringify(response));
+
+  if (response.status == "403" || response.status == "404") {
+    let request: CloudFrontRequest = event.Records[0].cf.request;
+    const imageUri: string = request.uri.substring(1);
+
+    const match = imageUri.match(/(.*)\/(\d+)x(\d+)\/(.*)\.(.*)/);
+
+    // Invalid request.
+    if (match == null) {
+      response.body = "Image not found";
+      response.bodyEncoding = "text";
+      return response;
+    }
+
+    const prefix: string = match[1];
+    const width: number = parseInt(match[2]);
+    const height: number = parseInt(match[3]);
+    const imageName: string = match[4];
+    const extension: string = match[5];
+
+    const originalKey = `${prefix}/${imageName}.${extension}`;
+
+    try {
+    } catch (e: any) {}
+
+    const originalImage = await s3Client
+      .getObject({ Bucket: "bts-main-images-bucket", Key: originalKey })
+      .promise();
+  }
+
   return response;
 };
